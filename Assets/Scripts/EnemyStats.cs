1966f322a702ca17e0fd6880c5dc31d988ec7f9c
markwright2002 <mark.wright8@ontariotechu.net>
2023-02-04 14:28:22 -0500

    added contatiner for enemy data

---
+++ Assets/Scripts/EnemyStats.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class EnemyStats : ScriptableObject
+{
+    public float speed = 1;
+    public int health = 1;
+    public int damage = 1;
+}
