56e2adb5385ea2fae563d222e2dfb4f85337e345
Gnaxxx <janvincentcruz9602@gmail.com>
2023-02-04 02:04:46 -0500

    Updated Enemy AI and Player Health

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -4,28 +4,43 @@ using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour
 {
-    public GameObject target;
-    public float range;
-    public float speed;
+    Transform target;
+    //private float range = 500.0f;
+    Rigidbody2D rb;
+    private float speed = 3.5f;
+    Vector2 moveDirection;
     // Start is called before the first frame update
-    void MoveTowardPlayer()
-    {
-        float dist = Vector3.Distance(target.transform.position, transform.position);
-        if (dist <= range)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed);
-        }
-    }
 
-    void Start()
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
+
+    void Start()
+    {
+        target = GameObject.Find("Character").transform;
+    }
+
     void Update()
     {
-        MoveTowardPlayer();
+        //if (dist <= range)
+        //{
+        //transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed);
+        if (target)
+        {
+            Vector3 direction = (target.position - transform.position).normalized;
+            moveDirection = direction;
+        }            
+        //}
+    }
 
+    void FixedUpdate()
+    {
+        if (target)
+        {
+            rb.velocity = new Vector2(moveDirection.x, moveDirection.y) * speed;
+        }
     }
 }
