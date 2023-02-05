2b08313ef00c07acc0b63ad33a95aee3bbda84b5
markwright2002 <mark.wright8@ontariotechu.net>
2023-02-05 14:46:13 -0500

    a

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -8,17 +8,20 @@ public class PlayerStats : MonoBehaviour
     public GameObject[] enemies; 
     private float currentHealth = 0.0f;
     private float maxHealth = 100.0f;
+    public GameObject HealthBar;
     // Start is called before the first frame update
     
     
     public void takeDamage(float dmg)
     {
         currentHealth -= dmg;
+        HealthBar.transform.localScale = new Vector3(0.01f * currentHealth, 1.0f, 1.0f);
     }
 
     void Start()
     {
         currentHealth = maxHealth;
+        HealthBar.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
     }
     //
 
